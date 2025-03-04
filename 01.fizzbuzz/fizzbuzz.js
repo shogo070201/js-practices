42ed33edae0625e768ebf84df55ccddf5902b20d
shogo070201 <shogo070201@gmail.com>
2025-03-05 01:03:57 +0900

    fizzbuzz課題の提出

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(String(i)); //数字が色付きで出力されるので、文字列に変換して出力する。
+  }
+}
